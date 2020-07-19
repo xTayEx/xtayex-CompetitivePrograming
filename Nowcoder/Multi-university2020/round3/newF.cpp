aa20edd722088dd639a7dcee10edb722b37d65dc
xtayex <75450574@qq.com>
2020-07-19 16:40:57 +0800

    2020-07-19,16:40:57

--- Nowcoder/Multi-university2020/round3/newF.cpp
+++ Nowcoder/Multi-university2020/round3/newF.cpp
@@ -20,6 +20,30 @@ const int maxn = 2e6 + 5;
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
+vector<int> vec;
+bool isnprime[maxn + 10];
+void init()
+{
+    mst(isnprime, 0);
+    vec.clear();
+    int bnd = sqrt(maxn);
+    for (int i = 2; i <= bnd; i++) {
+        if (!isnprime[i]) {
+            for (int j = i + i; j <= maxn; j += i) {
+                isnprime[j] = 1;
+            }
+        }
+    }
+    for (int i = 2; i <= maxn; i++) {
+        if (!isnprime[i]) {
+            vec.push_back(i);
+        }
+    }
+}
+ll gcd(ll a, ll b)
+{
+    return !b ? a : gcd(b, a % b);
+}
 ll exgcd(ll a, ll b, ll& x, ll& y)
 {
     if (a == 0 && b == 0)
@@ -33,37 +57,12 @@ ll exgcd(ll a, ll b, ll& x, ll& y)
     y -= a / b * x;
     return d;
 }
-ll gcd(ll a, ll b)
-{
-    return !b ? a : gcd(b, a % b);
-}
-bool isnprime[maxn + 20];
-vector<int> vec;
-void getprime()
-{
-    mst(isnprime, 0);
-    int bnd = (int)sqrt(maxn);
-    bnd++;
-    for (int i = 2; i <= bnd; i++) {
-        if (!isnprime[i]) {
-            for (int j = i * i; j <= maxn; j += i) {
-                isnprime[j] = 1;
-            }
-        }
-    }
-    for (int i = 2; i <= maxn; i++) {
-        if (!isnprime[i]) {
-            vec.push_back(i);
-        }
-    }
-}
 int main()
 {
+    init();
+    int len = vec.size();
     int T;
     scanf("%d", &T);
-    vec.clear();
-    getprime();
-    int tot = vec.size();
     while (T--) {
         ll a, b;
         scanf("%lld %lld", &a, &b);
@@ -72,29 +71,27 @@ int main()
             printf("%lld %lld %lld %lld\n", ((a / g) + 1), b / g, 1LL, b / g);
         } else if (g == 1) {
             ll tmpb = b;
-            ll res = 1LL;
-            for (int i = 0; 1LL * vec[i] * vec[i] <= tmpb && i < tot; i++) {
+            ll fact = 1LL;
+            for (int i = 0; vec[i] * vec[i] <= tmpb && i < len; i++) {
                 if (tmpb % vec[i] == 0) {
                     while (tmpb % vec[i] == 0) {
+                        fact *= vec[i];
                         tmpb /= vec[i];
-                        res *= vec[i];
                     }
                     break;
                 }
             }
-            if (tmpb > 1 && res != 1) {
-                ll d = res;
+            if (tmpb != 1 && fact != 1) {
+                ll d = fact;
                 ll f = tmpb;
                 ll c, e;
                 exgcd(f, d, c, e);
-                //printf("c = %lld, e = %lld\n",c,e);
-                c *= a;
-                e *= a;
-                if (c > 0 && e < 0) {
-                    printf("%lld %lld %lld %lld\n", c, d, -e, f);
-                } else {
-                    printf("%lld %lld %lld %lld\n", e, f, -c, d);
-                }
+                e = -e;
+                while (e < 0 || c < 0)
+                    e += f, c += d;
+                ll mul = a / gcd(f, d);
+                e *= mul, c *= mul;
+                printf("%lld %lld %lld %lld\n", c, d, e, f);
             } else {
                 puts("-1 -1 -1 -1");
             }
