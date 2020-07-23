fd3aef4a0c868b0755eae32bfadb220c6a05e5af
xtayex <75450574@qq.com>
2020-07-23 19:56:17 +0800

    2020-07-23,19:56:17

--- HDU/Hangdian-Multi-University/round1/I.cpp
+++ HDU/Hangdian-Multi-University/round1/I.cpp
@@ -23,53 +23,67 @@ using pll = pair<long long,long long>;
 typedef long long ll;
 typedef unsigned long long ull;
 struct line{
-    ll a,p;
+    ll p,a;
     bool operator<(const line& l)const{
         if(a!=l.a) return a>l.a;
-        return p<l.p;
+        return p>l.p;
     }
-    bool operator==(const line& l)const{
-        return a==l.a&&p==l.p;
-    }
+    ll cnt;
 };
 line ls[50005];
-line newls[5005];
-int stk[50005];
+int stk[50005<<1];
 double crossx(int idx1,int idx2)
 {
     double up=1.0*(ls[idx2].p-ls[idx1].p);
     double down=1.0*(ls[idx1].a-ls[idx2].a);
     return up/down;
 }
-map<pll,bool> vis;
+map<pll,int> vis;
 int main()
 {
     int T;scanf("%d",&T);
     while(T--){
-        int n;scanf("%d",&n);
-        ll a,p;
-        for(int i=1;i<=n;i++){
-            scanf("%lld %lld",&a,&p);
-            ls[i]={a,p};
-        }
-        sort(ls+1,ls+1+n);
-        int idx=1;
         vis.clear();
+        int n;scanf("%d",&n);
+        ll p,a;
+        ll  maxi=0;
+        int idx=0;
         for(int i=1;i<=n;i++){
-            pll pa=make_pair(ls[i].a, ls[i].p);
-            if(vis.count(pa)==0){
-                newls[idx++]=ls[i];
-                vis[pa]=1;
+            scanf("%lld %lld",&p,&a);
+            maxi=max(maxi,p);
+            pll pa={p,a};
+            if(vis[pa]==0){
+                idx++;
+                ls[idx]={p,a,1};
+                vis[pa]=idx;
+            }else{
+                ls[vis[pa]].cnt++;
             }
         }
         int Top=1;
+        ls[++idx]={maxi,-1,1};
+        //printf("idx = %d\n",idx);
+        sort(ls+1,ls+1+idx);
         stk[Top++]=1;
-        stk[Top++]=2;
-        for(int i=3;i<=n;i++){
-            while(Top>=3&&crossx(stk[Top-1],stk[Top-2])<crossx(stk[Top-1],i)) Top--;
+        for(int i=2;i<=idx;i++){
+            if(ls[i].a==ls[i-1].a) continue;
+            while(Top>=3&&crossx(stk[Top-1], stk[Top-2])-crossx(stk[Top-1],i)<=1e-8){
+                //printf("ls[stk[Top-1]].p = %lld ls[stk[Top-1]].a = %lld\n",ls[stk[Top-1]].p,ls[stk[Top-1]].a);
+                //printf("ls[stk[Top-2]].p = %lld ls[stk[Top-2]].a = %lld\n",ls[stk[Top-2]].p,ls[stk[Top-2]].a);
+                //printf("ls[i].p = %lld ls[i].a = %lld\n",ls[i].p,ls[i].a);
+                Top--;
+            }
             stk[Top++]=i;
+            //printf("i = %d, Top = %d\n",i,Top);
         }
-        printf("%d\n",Top-1);
+        //printf("Top = %d\n",Top);
+        ll ans=0;
+        for(int i=1;i<Top;i++){
+            if(ls[stk[i]].cnt<2){
+                ans++;
+            }
+        }
+        printf("%lld\n",ans-1);
     }
     return 0;
 }
