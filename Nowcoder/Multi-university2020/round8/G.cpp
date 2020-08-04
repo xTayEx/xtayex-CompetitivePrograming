96545b5c311cd8fdd0900b5b5bb409338200fd37
xtayex <75450574@qq.com>
2020-08-04 10:05:32 +0800

    2020-08-04,10:05:32

--- Nowcoder/Multi-university2020/round8/G.cpp
+++ Nowcoder/Multi-university2020/round8/G.cpp
@@ -22,13 +22,9 @@ typedef long long ll;
 typedef unsigned long long ull;
 struct node {
     int val[4];
-    int nodeval;
 };
 node ns[256 + 5];
-char s[105];
 unordered_map<string, int> mp1, mp2, mp3, mp4;
-unordered_map<int, int> Mp;
-vector<int> vec;
 inline void init()
 {
     mp1["one"] = 1, mp1["two"] = 2, mp1["three"] = 3, mp1["*"] = 4;
@@ -36,24 +32,41 @@ inline void init()
     mp3["solid"] = 1, mp3["striped"] = 2, mp3["open"] = 3, mp3["*"] = 4;
     mp4["red"] = 1, mp4["green"] = 2, mp4["purple"] = 3, mp4["*"] = 4;
 }
-int trans(int v1, int v2, int v3, int v4)
+char s1[15], s2[15], s3[15], s4[15];
+void solve(int n)
 {
-    int ret = v1 * 1000 + v2 * 100 + v3 * 10 + v4;
-    return ret;
-}
-void dfs(int cur, int now, node& n1, node& n2)
-{
-    if (cur >= 4) {
-        vec.push_back(now);
-        return;
+    for (int i = 1; i <= n; i++) {
+        for (int j = i + 1; j <= n; j++) {
+            for (int k = j + 1; k <= n; k++) {
+                const node& n1 = ns[i];
+                const node& n2 = ns[j];
+                const node& n3 = ns[k];
+                int cnt = 0;
+                for (int idx = 0; idx < 4; idx++) {
+                    if (n1.val[idx] != 4 && n2.val[idx] != 4) {
+                        if (n1.val[idx] == n2.val[idx]) {
+                            if (n2.val[idx] == n3.val[idx] || n3.val[idx] == 4) {
+                                cnt++;
+                            }
+                        } else {
+                            if (n3.val[idx] == 6 - n1.val[idx] - n2.val[idx] || n3.val[idx] == 4) {
+                                cnt++;
+                            }
+                        }
+                    } else if ((n1.val[idx] != 4 && n2.val[idx] == 4) || (n1.val[idx] == 4 && n2.val[idx] != 4)) {
+                        cnt++;
+                    } else {
+                        cnt++;
+                    }
+                }
+                if (cnt == 4) {
+                    printf("%d %d %d\n", i, j, k);
+                    return;
+                }
+            }
+        }
     }
-    if (n1.val[cur] == n2.val[cur]) {
-        dfs(cur + 1, now * 10 + n1.val[cur], n1, n2);
-        dfs(cur + 1, now * 10 + 5, n1, n2);
-    } else {
-        dfs(cur + 1, now * 10 + (6 - n1.val[cur] - n2.val[cur]), n1, n2);
-        dfs(cur + 1, now * 10 + 5, n1, n2);
-    }
+    puts("-1");
 }
 int main()
 {
@@ -62,71 +75,18 @@ int main()
     scanf("%d", &T);
     int kase = 0;
     while (T--) {
-        printf("Case #%d: ", ++kase);
         int N;
         scanf("%d", &N);
         for (int i = 1; i <= N; i++) {
-            scanf("%s", s + 1);
-            int len = strlen(s + 1);
-            int lef = 1;
-            int rig = 1;
-            string tmp;
-            int Val = 0;
-            int lefcnt = 1;
-            int nsidx = 1;
-            Mp.clear();
-            while (lef <= rig && lef <= len && rig <= len) {
-                while (s[rig] != ']')
-                    rig++;
-                tmp.clear();
-                for (int i = lef; i <= rig; i++) {
-                    tmp.push_back(s[i]);
-                }
-                if (lefcnt == 1) {
-                    ns[nsidx].val[0] = mp1[tmp];
-                    Val = Val * 10 + mp1[tmp];
-                } else if (lefcnt == 2) {
-                    ns[nsidx].val[1] = mp2[tmp];
-                    Val = Val * 10 + mp2[tmp];
-                } else if (lefcnt == 3) {
-                    ns[nsidx].val[2] = mp3[tmp];
-                    Val = Val * 10 + mp3[tmp];
-                } else if (lefcnt == 4) {
-                    ns[nsidx++].val[3] = mp4[tmp];
-                    Val = Val * 10 + mp4[tmp];
-                    ns[nsidx - 1].nodeval = Val;
-                    Mp[Val] = i;
-                    Val = 0;
-                }
-                lef = rig + 1;
-                rig = lef;
-                lefcnt++;
-            }
+            getchar();
+            scanf("[%99[^]]][%99[^]]][%99[^]]][%99[^]]]", s1 + 1, s2 + 1, s3 + 1, s4 + 1);
+            ns[i].val[0] = mp1[string(s1 + 1)];
+            ns[i].val[1] = mp2[string(s2 + 1)];
+            ns[i].val[2] = mp3[string(s3 + 1)];
+            ns[i].val[3] = mp4[string(s4 + 1)];
         }
-        bool flag = 0;
-        for (int i = 1; i <= N; i++) {
-            for (int j = i + 1; j <= N; j++) {
-                int todo = 0;
-                node& n1 = ns[i];
-                node& n2 = ns[j];
-                vec.clear();
-                dfs(0, 0, n1, n2);
-                int sz = vec.size();
-                for (int k = 0; k < sz; k++) {
-                    if (Mp[vec[k]]) {
-                        printf("%d %d %d\n", i, j, Mp[vec[k]]);
-                        flag = 1;
-                        break;
-                    }
-                }
-                if (flag)
-                    break;
-            }
-            if (flag)
-                break;
-        }
-        if (!flag)
-            puts("-1");
+        printf("Case #%d: ", ++kase);
+        solve(N);
     }
     return 0;
 }
