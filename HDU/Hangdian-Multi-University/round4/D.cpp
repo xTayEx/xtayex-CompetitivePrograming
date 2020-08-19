a9a463ee8968be39550d82b492816098626fcbd5
xtayex <75450574@qq.com>
2020-08-19 22:43:09 +0800

    2020-08-19,22:43:09

--- HDU/Hangdian-Multi-University/round4/D.cpp
+++ HDU/Hangdian-Multi-University/round4/D.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <cstdio>
 #include <cstring>
+#include <functional>
 #include <iostream>
 #include <map>
 #include <queue>
@@ -14,53 +15,104 @@
 #define mst(a, b) memset((a), (b), sizeof(a))
 #define idebug(x) printf("%s = %d\n", #x, x)
 #define ddebug(x) printf("%s = %lf\n", #x, x)
-#define INF 0x3f3f3f3f
+#define INF 1LL << 45
 #define lson lef, mid, rt << 1
 #define rson mid + 1, rig, rt << 1 | 1
-const int maxn = 1e5 + 5;
+const int maxn = 2e5 + 5;
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
-vector<int> G[maxn];
-char type[maxn];
-vector<int> id[maxn];
+char dir[maxn], newdir[maxn];
+vector<int> ID[maxn];
+ll dijdis[maxn];
+bool vis[maxn];
 struct edge {
-    int u, v, dis;
-    edge(int u, int v, int dis)
-        : u(u)
-        , v(v)
-        , dis(dis)
+    int v;
+    ll dis;
+    edge(int v, ll dis)
     {
+        this->v = v, this->dis = dis;
+    }
+    bool operator>(const edge& e) const
+    {
+        return dis > e.dis;
     }
 };
-vector<edge> es;
+vector<edge> G[maxn];
+priority_queue<edge, vector<edge>, greater<edge>> que;
+void dijkstra(int st, int n)
+{
+    fill(dijdis, dijdis + n + 1, INF);
+    fill(vis, vis + n + 1, 0);
+    while (!que.empty()) {
+        que.pop();
+    }
+    dijdis[st] = 0, que.push({ st, dijdis[st] });
+    while (!que.empty()) {
+        int u = que.top().v;
+        que.pop();
+        if (vis[u]) {
+            continue;
+        }
+        vis[u] = 1;
+        int len = G[u].size();
+        for (int i = 0; i < len; i++) {
+            int v = G[u][i].v;
+            int D = G[u][i].dis;
+            if (!vis[v] && dijdis[v] > dijdis[u] + D) {
+                dijdis[v] = dijdis[u] + D;
+                que.push({ v, dijdis[v] });
+            }
+        }
+    }
+}
 int main()
 {
     int T;
     scanf("%d", &T);
+    int n, m, s, t, x;
     while (T--) {
-        int n, m, s, t;
-        ll x;
-        scanf("%d%d%d%d%lld", &n, &m, &s, &t, &x);
-        scanf("%s", type + 1);
-        int a, b, d;
+        scanf("%d%d%d%d%d", &n, &m, &s, &t, &x);
+        scanf("%s", dir + 1);
         int tot = 0;
-        for (int i = 1; i <= m; i++) {
-            scanf("%d%d%d", &a, &b, &d);
-            es.emplace_back(a, b, d);
-        }
         for (int i = 1; i <= n; i++) {
-            id[i].clear();
-            if (type[i] == 'M') {
-                id[i].emplace_back(++tot);
-                id[i].emplace_back(++tot);
+            ID[i].clear();
+            if (dir[i] == 'M') {
+                ID[i].emplace_back(tot + 1);
+                ID[i].emplace_back(tot + 2);
+                newdir[tot + 1] = 'L', newdir[tot + 2] = 'R';
+                tot += 2;
             } else {
-                id[i].emplace_back(++tot);
+                ID[i].emplace_back(tot + 1);
+                newdir[tot + 1] = dir[i];
+                tot++;
             }
         }
-        int siz;
-        for (int i = 1; i <= m; i++) {
-            
+        for (int i = 0; i <= tot; i++) {
+            G[i].clear();
         }
+        int a, b, d;
+        for (int i = 1; i <= m; i++) {
+            scanf("%d%d%d", &a, &b, &d);
+            for (auto node1 : ID[a]) {
+                for (auto node2 : ID[b]) {
+                    ll dis = d + (newdir[node1] == newdir[node2] ? 0 : x);
+                    G[node1].emplace_back(node2, dis);
+                    G[node2].emplace_back(node1, dis);
+                }
+            }
+        }
+        int ss = 0, tt = ++tot;
+        for (auto it : ID[s]) {
+            G[it].emplace_back(ss, 0);
+            G[ss].emplace_back(it, 0);
+        }
+        for (auto it : ID[t]) {
+            G[it].emplace_back(tt, 0);
+            G[tt].emplace_back(it, 0);
+        }
+        dijkstra(ss, tot + 1);
+        printf("%lld\n", dijdis[tt]);
     }
+    return 0;
 }
---
+++ HDU/Hangdian-Multi-University/round4/G.cpp
@@ -0,0 +1,134 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#define mst(a, b) memset((a), (b), sizeof(a))
+#define debug(x) cerr << #x << " " << x << "\n"
+#define INF 0x3f3f3f3f
+#define lson lef, mid, rt << 1
+#define rson mid + 1, rig, rt << 1 | 1
+const int maxn = 2e5 + 5;
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+ll t[maxn], x[maxn];
+vector<int> G[maxn];
+unordered_map<ll, int> lvis;
+inline void addedge(int u, int v)
+{
+    G[u].emplace_back(v);
+    G[v].emplace_back(u);
+}
+bool used[maxn];
+int lefcnt, rigcnt, dis;
+int dx[maxn], dy[maxn], cx[maxn], cy[maxn];
+int tot;
+bool bfs()
+{
+    queue<int> que;
+    dis = INF;
+    mst(dx, -1), mst(dy, -1);
+    for (int i = 1; i <= lefcnt; i++) {
+        if (cx[i] == -1) {
+            que.push(i);
+            dx[i] = 0;
+        }
+    }
+    while (!que.empty()) {
+        int u = que.front();
+        que.pop();
+        if (dx[u] > dis) break;
+        for (auto& v : G[u]) {
+            if (dy[v] == -1) {
+                dy[v] = dx[u] + 1;
+                if (cy[v] == -1)
+                    dis = dy[v];
+                else
+                    dx[cy[v]] = dy[v] + 1, que.push(cy[v]);
+            }
+        }
+    }
+    return dis != INF;
+}
+int dfs(int u)
+{
+    for (auto v : G[u]) {
+        if (!used[v] && dy[v] == dx[u] + 1) {
+            used[v] = 1;
+            if (cy[v] != -1 && dy[v] == dis) continue;
+            if (cy[v] == -1 || dfs(cy[v])) {
+                cy[v] = u, cx[u] = v;
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+int hopcroft_karp()
+{
+    int res = 0;
+    mst(cx, -1), mst(cy, -1);
+    while (bfs()) {
+        mst(used, 0);
+        for (int i = 1; i <= lefcnt; i++) {
+            if (cx[i] == -1) {
+                res += dfs(i);
+            }
+        }
+    }
+    return res;
+}
+inline void init(int n)
+{
+    for (int i = 0; i <= 2 * n; i++) {
+        G[i].clear();
+    }
+}
+vector<int> vi;
+int getid(int x)
+{
+    int ret = distance(vi.begin(), lower_bound(vi.begin(), vi.end(), x)) + 1;
+    return ret;
+}
+int main()
+{
+    int T;
+    scanf("%d", &T);
+    while (T--) {
+        vi.clear(), lvis.clear();
+        int n;
+        scanf("%d", &n);
+        init(n);
+        tot = 0;
+        lefcnt = rigcnt = 0;
+        for (int i = 1; i <= n; i++) {
+            scanf("%lld%lld", &t[i], &x[i]);
+            ll tmp1 = x[i] - t[i];
+            ll tmp2 = x[i] + t[i];
+            vi.push_back(tmp1), vi.push_back(tmp2);
+        }
+        sort(vi.begin(), vi.end());
+        vi.erase(unique(vi.begin(), vi.end()), vi.end());
+        lefcnt = distance(vi.begin(), vi.end());
+        for (int i = 1; i <= n; i++) {
+            ll tmp1 = x[i] - t[i];
+            ll tmp2 = x[i] + t[i];
+            int id1 = getid(tmp1);
+            int id2 = getid(tmp2);
+            G[id1].emplace_back(id2);
+        }
+        int ans = hopcroft_karp();
+        printf("%d\n", ans);
+    }
+    return 0;
+}
