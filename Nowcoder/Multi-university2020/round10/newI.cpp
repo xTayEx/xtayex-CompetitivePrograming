35c02017a52f7ae0c9d4a8e3559498e9e3dcb11d
xtayex <75450574@qq.com>
2020-08-26 17:15:54 +0800

    2020-08-26,17:15:54

--- Nowcoder/Multi-university2020/round10/newI.cpp
+++ Nowcoder/Multi-university2020/round10/newI.cpp
@@ -12,8 +12,7 @@
 #include <unordered_map>
 #include <vector>
 #define mst(a, b) memset((a), (b), sizeof(a))
-#define idebug(x) printf("%s = %d\n", #x, x)
-#define ddebug(x) printf("%s = %lf\n", #x, x)
+#define debug(x) cerr << #x << " = " << x << "\n"
 #define INF 0x3f3f3f3f
 #define lson lef, mid, rt << 1
 #define rson mid + 1, rig, rt << 1 | 1
@@ -21,6 +20,8 @@ const int maxn = 1e5 + 5;
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
+using pii = pair<int, int>;
+map<pii, bool> mp;
 int main()
 {
     int T;
@@ -28,19 +29,19 @@ int main()
     while (T--) {
         int n;
         scanf("%d", &n);
+        mp.clear();
         for (int i = 2; i <= n / 2; i++) {
             for (int j = 1; j < i; j++) {
                 printf("%d %d\n", j, i);
             }
         }
-        for (int i = n / 2 + 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-                printf("%d %d\n", j, i);
-            }
-        }
         for (int i = 1; i <= n / 2; i++) {
-            for (int j = n - i + 1; j <= n; j++) {
-                printf("%d %d\n", i, j);
+            for (int j = n / 2 + 1; j <= n; j++) {
+                pii now = { i, j };
+                if (mp.count(now) == 0) {
+                    printf("%d %d\n", i, j);
+                    mp[now] = 1;
+                }
             }
         }
         for (int i = n / 2 + 1; i < n; i++) {
