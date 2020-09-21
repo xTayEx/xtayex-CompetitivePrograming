0308d30e0cba034ce9fcf797203d5d9a0d0de89b
xtayex <75450574@qq.com>
2020-09-21 12:52:13 +0800

    2020-09-21,12:52:12

--- Codeforces/cf1318B.cpp
+++ Codeforces/cf1318B.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <cstdio>
 #include <cstring>
+#include <functional>
 #include <iostream>
 #include <map>
 #include <queue>
@@ -16,30 +17,39 @@
 #define INF 0x3f3f3f3f
 #define lson lef, mid, rt << 1
 #define rson mid + 1, rig, rt << 1 | 1
-const int maxn = 2e5 + 5;
+const int maxn = 1e5 + 5;
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
-int dp[maxn][2];
-int a[maxn];
+int a[maxn], l[maxn];
+vector<int> unl, ans;
 int main()
 {
     int T;
     scanf("%d", &T);
     while (T--) {
         int n;
+        ans.clear(), unl.clear();
         scanf("%d", &n);
         for (int i = 1; i <= n; i++) {
             scanf("%d", &a[i]);
         }
-        dp[1][0] = a[1], dp[1][1] = INF;
-        dp[2][0] = a[1] + a[2], dp[2][1] = dp[1][0];
-        for (int i = 3; i <= n; i++) {
-            dp[i][0] = min(dp[i - 1][1] + a[i], dp[i - 2][1] + a[i - 1] + a[i]);
-            dp[i][1] = min(dp[i - 1][0], dp[i - 2][0]);
+        for (int i = 1; i <= n; i++) {
+            scanf("%d", &l[i]);
+            if (l[i] == 0) unl.push_back(a[i]);
         }
-        int ans = min(dp[n][0], dp[n][1]);
-        printf("%d\n", ans);
+        sort(unl.begin(), unl.end(), greater<int>());
+        int idx = 0;
+        for (int i = 1; i <= n; i++) {
+            if (l[i] == 1)
+                ans.push_back(a[i]);
+            else
+                ans.push_back(unl[idx++]);
+        }
+        for (auto& it : ans) {
+            printf("%d ", it);
+        }
+        puts("");
     }
     return 0;
 }
